7c076b844f996777aaf0d38cf878f10bc481edb9
psyduuuuck <70102297+psyduuuuck@users.noreply.github.com>
2020-08-27 01:03:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ var config = {
         "愿你成为四岁时想要成为的样子",
         "我会一直在你身边",
         "生日快乐",
-        "muuuuuuuuuuuuuuuuua",
+        "muuuuuuuuua",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -34,18 +34,35 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "祝你的梦境温柔缱绻": "./imgs/mengjing.jpg",
+                "祝你的枕头蓬松柔软": "./imgs/zhentou.jpg",
+                "祝你抬起头来能看到星星": "./imgs/xingxing.jpg",
+                "祝你在旧衣服里找到零钱": "./imgs/lingqian.jpg",
+                "祝你能抢到演唱会最好的票": "./imgs/yanchanghui.jpg",
+                "祝你喜欢的卷饼店从不收摊": "./imgs/juanbingdian.jpg",
+                "祝你每次洗澡都是刚好的水温": "./imgs/xizao.jpg",
+                "祝你所有的期待都能变成实现": "./imgs/qidai.jpg",
+                "祝你摇摇晃晃的冰淇淋球从不掉落": "./imgs/bingqilin.jpg",
+                "祝你永远吃到冰西瓜中心的一口甜": "./imgs/xigua.jpg",
+                "祝你总能从冰箱里找到最后一支雪糕": "./imgs/xuegao.jpg",
+                "祝你遇到的每只狗狗都向你摇晃尾巴": "./imgs/gougou.jpg",
+                "那我愿你过得简单": "./imgs/jiandan.jpg",
+                "愿你永远对世界保持好奇": "./imgs/haoqi.jpg",
+                "愿你成为四岁时想要成为的样子": "./imgs/sisui.jpg",
+                "我会一直在你身边": "./imgs/shenbian.jpg",
+         "生日快乐": "./imgs/shengrikuaile.jpg",
+         "muuuuuuuuua": "./imgs/mua.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "大头唱歌",
+        bannar_coming: "大头放气球",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        cake_fadein: "蛋糕！",
+        light_candle: "蜡烛！",
+        wish_message: "生日快乐！",
+        story: "这里有您的一条留言",
     }
 };
